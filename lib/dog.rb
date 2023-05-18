18ecd2c5cebb77792b0b4a117b379b966acec20f
Khalid <khaliddakar001@gmail.com>
2023-05-18 10:00:02 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+       # Class body
+     
+       # Instance Method Definition
+       def bark
+         puts "Woof!"
+       end
+     end
+
+     class Dog
+       def sit
+         puts "The Dog is sitting"
+       end
+     end
+
+   
